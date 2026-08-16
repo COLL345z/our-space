ab8a7650727777def79ef568dc3f4ad6abca67b2
COLL345z <muoki345@gmail.com>
2026-08-16 15:45:02 +0300

    Update MemoryRepository.java

--- src/main/java/com/example/demo/repository/MemoryRepository.java
+++ src/main/java/com/example/demo/repository/MemoryRepository.java
@@ -2,6 +2,11 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Memory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
+@Repository
 public interface MemoryRepository extends JpaRepository<Memory, Long> {
+    List<Memory> findByCategory(String category);
+    List<Memory> findByCategoryOrderByDateCreatedDesc(String category);
 }
